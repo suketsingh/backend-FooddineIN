964067958bf9c870f2239eafec53a3f6da2e0457
Suket Singh <47017166+suketsingh@users.noreply.github.com>
2019-11-25 20:49:29 -0500

    Update CandidateController.java

--- src/main/java/com/mynotes/spring/data/mongodb/CandidateController.java
+++ src/main/java/com/mynotes/spring/data/mongodb/CandidateController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mynotes.spring.data.mongodb;
+
 
 @RestController
 @RequestMapping("/candidate")
