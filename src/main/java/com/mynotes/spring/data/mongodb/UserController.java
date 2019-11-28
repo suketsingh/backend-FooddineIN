8cff8ad2fa252f4e095962d4c4ef020059b460b7
Suket Singh <47017166+suketsingh@users.noreply.github.com>
2019-11-27 21:34:39 -0500

    Update UserController.java

--- src/main/java/com/mynotes/spring/data/mongodb/UserController.java
+++ src/main/java/com/mynotes/spring/data/mongodb/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
