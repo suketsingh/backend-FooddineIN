fa31d97ef429608edcd2eb8e0e1fc0f76f389b3c
Suket Singh <ssingh45@syr.edu>
2019-11-27 21:02:01 -0500

    Update UserController.java

--- src/main/java/com/mynotes/spring/data/mongodb/UserController.java
+++ src/main/java/com/mynotes/spring/data/mongodb/UserController.java
@@ -46,8 +46,8 @@ public class UserController {
     public User update(@PathVariable String id, @RequestBody User updatedUser) {
         User user = userRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException());
-        user.setName(updatedUser.getFirstName());
-        user.setName(updatedUser.getLastName());
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
         user.setEmail(updatedUser.getEmail());
         return userRepository.save(user);
     }
@@ -73,7 +73,7 @@ public class UserController {
     public List<User> searchByName(@RequestParam(name = "firstName") String firstName) {
         List<User> result = new ArrayList<>();
         
-        result = userRepository.findBygfirstName(firstName);
+        result = userRepository.findByfirstName(firstName);
         
         return result;
     }
